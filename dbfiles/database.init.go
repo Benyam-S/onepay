639fd17405ebb3529fa310dc801df0296ce96bb5
Benyam Simayehu <50404694+Biny-BK@users.noreply.github.com>
2020-06-22 13:11:27 +0300

    database init

--- dbfiles/database.init.go
+++ dbfiles/database.init.go
@@ -2,16 +2,16 @@ package dbfiles
 
 // User is a type that defines a OnePay user
 type User struct {
-	UserID      string
-	FirstName   string
-	LastName    string
-	Email       string
-	PhoneNumber string
+	UserID      string `gorm:"primary_key; unique; not null"`
+	FirstName   string `gorm:"not null"`
+	LastName    string `gorm:"not null"`
+	Email       string `gorm:"not null"`
+	PhoneNumber string `gorm:"not null"`
 }
 
 // UserPassword is a type that defines a OnePay user password
 type UserPassword struct {
-	UserID   string
-	Password string
-	Salt     string
+	UserID   string `gorm:"primary_key; unique; not null"`
+	Password string `gorm:"not null"`
+	Salt     string `gorm:"not null"`
 }
--- dbfiles/user.sql
+++ dbfiles/user.sql
@@ -1,9 +1,9 @@
 CREATE TABLE user (
     user_id VARCHAR(255) PRIMARY KEY UNIQUE NOT NULL,
-    first_name VARCHAR(255),
+    first_name VARCHAR(255) NOT NULL,
     last_name VARCHAR(255),
-    email VARCHAR(255),
-    phone_number VARCHAR(255)
+    email VARCHAR(255) NOT NULL,
+    phone_number VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE password(
