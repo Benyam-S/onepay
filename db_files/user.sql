6fd4779f6a551e0199fb98bcbf5fadde23d5069a
Benyam Simayehu <50404694+Biny-BK@users.noreply.github.com>
2020-06-22 12:42:14 +0300

    database init

--- db_files/user.sql
+++ db_files/user.sql
@@ -5,3 +5,9 @@ CREATE TABLE user (
     email VARCHAR(255),
     phone_number VARCHAR(255)
 );
+
+CREATE TABLE password(
+    user_id VARCHAR(255) PRIMARY KEY UNIQUE NOT NULL,
+    password VARCHAR(255),
+    salt VARCHAR(255)
+)
